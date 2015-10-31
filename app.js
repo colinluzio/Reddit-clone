2bc90603c192c2f455dfd41134725871873c6d08
Colin Luzio <colinluzio@yahoo.co.uk>
2015-10-31 16:24:05 +0000

    add routes

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+
+mongoose.connect('mongodb://localhost/news');
 var express = require('express');
 var http = require('http');
 var path = require('path');
@@ -5,26 +10,90 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes');
-var users = require('./routes/user');
-
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
 var app = express();
+var router = express.Router();
+var routes = require('./routes');
+//var users = require('./routes/user');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+router.get('/about', function(req, res) {
+    res.send('im the about page!'); 
+});
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
 
+    res.json(posts);
+  });
+});
+router.post('/posts', function(req, res, next) {
+
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+router.get('/posts/:post', function(req, res) {
+  req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+app.use('/', router);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(favicon());
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(app.router);
+
+//app.use(app.router);
 
 app.get('/', routes.index);
-app.get('/users', users.list);
+//app.get('/users', users.list);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
@@ -57,3 +126,8 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
